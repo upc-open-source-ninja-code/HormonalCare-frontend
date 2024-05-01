e41fa0a803ea8fdd1f3ee01ee428a9e31a082a87
Seba <sebalobato05@gmail.com>
2024-05-01 13:56:35 -0500

    Reason consultation
    feat(component): Add ReasonConsultation component to capture consultation reasons

---
+++ src/app/medical-history/components/reasonconsultation/reasonconsultation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reasonconsultation',
+  templateUrl: './reasonconsultation.component.html',
+  styleUrl: './reasonconsultation.component.css'
+})
+export class ReasonconsultationComponent {
+
+}
