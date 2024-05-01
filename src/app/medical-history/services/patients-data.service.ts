be356b577b7148a41292b676ebd6d6eaf4909b6f
Seba <sebalobato05@gmail.com>
2024-05-01 13:32:27 -0500

    Patients data service
    feat(service): Update PatientsDataService to access HTTP client

---
+++ src/app/medical-history/services/patients-data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import {BaseService} from "../../shared/services/base.service";
+import {PatientEntity} from "../../profiles/model/patient.entity";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class PatientsDataService extends BaseService<PatientEntity> {
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.basePath = 'http://localhost:3000';
+    this.resourceEndpoint = '/patients';
+  }
+
+  getPatientDetails(id: string): Observable<PatientEntity> {
+    return this.http.get<PatientEntity>(`${this.basePath}${this.resourceEndpoint}/${id}`);
+  }
+}
