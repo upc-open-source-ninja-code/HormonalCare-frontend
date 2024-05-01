258e25c15cf51b4bfebd41ded3084c813a499e23
Seba <sebalobato05@gmail.com>
2024-05-01 13:51:27 -0500

    Patient data
    feat(component): Implement PatientData component to display patient information

---
+++ src/app/medical-history/components/patientdata/patientdata.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {PatientsDataService} from "../../services/patients-data.service";
+import {PatientEntity} from "../../../profiles/model/patient.entity";
+
+@Component({
+  selector: 'app-patientdata',
+  templateUrl: './patientdata.component.html',
+  styleUrls: ['./patientdata.component.css']
+})
+export class PatientdataComponent implements OnInit {
+  patient: PatientEntity = new PatientEntity();
+
+  constructor(private patientsDataService: PatientsDataService) {}
+
+  ngOnInit() {
+    this.getPatientDetails('1'); // Reemplaza '1' con el ID del paciente que deseas obtener
+  }
+
+  getPatientDetails(id: string) {
+    this.patientsDataService.getPatientDetails(id)
+      .subscribe((data: PatientEntity) => {
+        this.patient = data;
+      }, error => {
+        console.log('Error fetching patient details:', error);
+      });
+  }
+}
