5ad2a0a5a89940e0ca4418f3fe09469f98e5c02e
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-05-17 21:42:56 -0500

    fix(doctor-chat component)
    fixing doctor chat component.

--- src/app/communications/pages/doctor-chat/doctor-chat.component.ts
+++ src/app/communications/pages/doctor-chat/doctor-chat.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '../../services/doctor.service'
+
 
 @Component({
   selector: 'app-doctor-chat',
   templateUrl: './doctor-chat.component.html',
-  styleUrl: './doctor-chat.component.css'
+  styleUrls: ['./doctor-chat.component.css']
 })
-export class DoctorChatComponent {
+export class DoctorChatComponent implements OnInit {
+  currentUserEmail: string = 'mariagonzalez@example.com';
+  selectedUserEmail: string = 'pedrosanchez@example.com';
+  doctors: any[] = [];
 
+  constructor(private doctorService: DoctorService) { }
+
+  ngOnInit(): void {
+    this.loadDoctors();
+  }
+
+  loadDoctors(): void {
+    this.doctorService.getDoctors().subscribe(data => {
+      this.doctors = data; // Asegúrate de que esto coincida con la estructura de tu JSON
+      console.log(this.doctors); // Agrega esto para verificar los datos
+    });
+  }
+
+  switchUser(user: string) {
+    this.currentUserEmail = user;
+  }
+
+  selectChatUser(user: string) {
+    this.selectedUserEmail = user;
+  }
 }
