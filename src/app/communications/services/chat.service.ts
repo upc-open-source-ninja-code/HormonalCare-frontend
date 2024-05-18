421d2505e096d73c09e6cd81a0e2aacca6a20e67
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-05-17 21:38:23 -0500

    chore(doctor and chat service)
    first commit of services

---
+++ src/app/communications/services/chat.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Message } from '../model/message';
+import { DoctorProfile } from '../model/doctor-profile';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<DoctorProfile[]> {
+    return this.http.get<DoctorProfile[]>(`${this.apiUrl}/doctors`);
+  }
+
+  getDoctorByEmail(email: string): Observable<DoctorProfile> {
+    return this.http.get<DoctorProfile[]>(`${this.apiUrl}/doctors?email=${email}`).pipe(
+      map(doctors => doctors[0]) // Asume que el correo electrónico es único y solo devuelve el primer doctor
+    );
+  }
+
+  getMessages(from: string, to: string): Observable<Message[]> {
+    return this.http.get<Message[]>(`${this.apiUrl}/messages?_sort=timestamp&_order=asc`).pipe(
+      map(messages => messages.filter(
+        message => (message.from === from && message.to === to) || (message.from === to && message.to === from)
+      ))
+    );
+  }
+
+  sendMessage(message: Message): Observable<Message> {
+    return this.http.post<Message>(`${this.apiUrl}/messages`, message);
+  }
+}
