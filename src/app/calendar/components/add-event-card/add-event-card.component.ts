df8a5cfa55d6bf62c4258693b43013d162427830
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-06-02 12:43:16 -0500

    feat(calendar-view): Added google-calendar-service and update add-event-calendar.

--- src/app/calendar/components/add-event-card/add-event-card.component.ts
+++ src/app/calendar/components/add-event-card/add-event-card.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-declare var gapi: any;
-declare var google: any;
+import {GoogleCalendarService} from "../../services/google-calendar-service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-add-event-card',
@@ -11,11 +10,8 @@ declare var google: any;
 })
 export class AddEventCardComponent implements OnInit {
   eventForm: FormGroup;
-  formValue: any;
-  startDateTime: Date;
-  endDateTime: Date;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private googleCalendarService: GoogleCalendarService, private snackBar: MatSnackBar) {
     this.eventForm = this.fb.group({
       eventDate: [null, Validators.required],
       startTime: [null, Validators.required],
@@ -26,65 +22,43 @@ export class AddEventCardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    try {
-      console.log('Initializing Google Identity Services...');
-      google.accounts.id.initialize({
-        client_id: '622160902930-4ll4iqmuupg55lsa1oapr5re055j6421.apps.googleusercontent.com',
-        callback: this.handleCredentialResponse.bind(this)
-      });
-      google.accounts.id.renderButton(
-        document.getElementById('buttonDiv'),
-        { theme: 'outline', size: 'large' }
-      );
-      google.accounts.id.prompt();
-      console.log('Google Identity Services initialized.');
-    } catch (error) {
-      console.error('Error initializing Google Identity Services:', error);
-    }
   }
 
   createEvent() {
     if (this.eventForm.valid) {
-      this.formValue = this.eventForm.value;
-      this.startDateTime = new Date(this.formValue.eventDate);
-      const [startHours, startMinutes] = this.formValue.startTime.split(':');
-      this.startDateTime.setHours(startHours, startMinutes);
+      const formValue = this.eventForm.value;
+      const startDateTime = new Date(formValue.eventDate);
+      const [startHours, startMinutes] = formValue.startTime.split(':');
+      startDateTime.setHours(startHours, startMinutes);
 
-      this.endDateTime = new Date(this.formValue.eventDate);
-      const [endHours, endMinutes] = this.formValue.endTime.split(':');
-      this.endDateTime.setHours(endHours, endMinutes);
+      const endDateTime = new Date(formValue.eventDate);
+      const [endHours, endMinutes] = formValue.endTime.split(':');
+      endDateTime.setHours(endHours, endMinutes);
 
-      console.log('Event Created:', this.formValue);
+      const eventDetails = {
+        startTime: startDateTime.toISOString(),
+        endTime: endDateTime.toISOString(),
+        email: formValue.patientEmail,
+        summary: formValue.title,
+        reminders: {
+          useDefault: false,
+          overrides: [
+            { method: 'email', minutes: 24 * 60 },
+            { method: 'popup', minutes: 10 },
+          ],
+        },
+      };
 
-      // Use the Google Calendar API to create an event.
-      gapi.client.calendar.events.insert({
-        calendarId: 'primary',
-        resource: {
-          summary: this.formValue.title,
-          start: {
-            dateTime: this.startDateTime.toISOString(),
-            timeZone: 'America/Lima'
-          },
-          end: {
-            dateTime: this.endDateTime.toISOString(),
-            timeZone: 'America/Lima'
-          },
-          attendees: [
-            { email: this.formValue.patientEmail }
-          ]
-        }
-      }).then((response: any) => {
-        console.log('Event created:', response);
-      }).catch((error: any) => {
-        console.error('Error creating event:', error);
+      this.googleCalendarService.createGoogleEvent(eventDetails).then(() => {
+        this.snackBar.open('Event created', '', {
+          duration: 4000,
+        });
+        this.eventForm.reset();
+      }).catch((error) => {
+        console.error('Error creating event', error);
       });
     } else {
-      console.error('Form is invalid');
+      console.error('Form is not valid');
     }
-  }
-
-  handleCredentialResponse(response: any) {
-    console.log('ID token:', response.credential);
-    // Aquí puedes proceder a autenticar al usuario en tu backend si es necesario.
   }
 }
