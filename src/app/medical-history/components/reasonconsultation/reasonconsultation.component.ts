d4f399aaf1e13d8b734ff5bdd83d4d06b0c6c08c
Luis Sebastian Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-06-23 16:44:33 -0500

    feat(reason of consultation): added post method in medical record

--- src/app/medical-history/components/reasonconsultation/reasonconsultation.component.ts
+++ src/app/medical-history/components/reasonconsultation/reasonconsultation.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReasonOfConsultationService } from '../../services/reason-of-consultation.service';
 
 @Component({
   selector: 'app-reasonconsultation',
   templateUrl: './reasonconsultation.component.html',
-  styleUrl: './reasonconsultation.component.css'
+  styleUrls: ['./reasonconsultation.component.css']
 })
-export class ReasonconsultationComponent {
+export class ReasonconsultationComponent implements OnInit {
+  description: string = '';
+  symptoms: string = '';
+  medicalRecordId: number = 2; // Aquí debes poner el ID del paciente
 
+  constructor(private reasonOfConsultationService: ReasonOfConsultationService) {}
+
+  ngOnInit() {
+    this.reasonOfConsultationService.getReasonOfConsultation(this.medicalRecordId).subscribe(data => {
+      this.description = data.description;
+      this.symptoms = data.symptoms;
+      this.medicalRecordId = data.medicalRecordId;
+    });
+  }
+
+  editReasonOfConsultation() {
+    const data = {
+      description: this.description,
+      symptoms: this.symptoms,
+      medicalRecordId: this.medicalRecordId
+    };
+
+    this.reasonOfConsultationService.postReasonOfConsultation(data).subscribe(response => {
+      console.log(response);
+      // Aquí puedes manejar la respuesta de la API
+    });
+  }
 }
---
+++ src/app/medical-history/services/reason-of-consultation.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReasonOfConsultationService {
+  private basePath = 'http://localhost:8080/api/v1';
+
+  constructor(private http: HttpClient) {}
+
+  getReasonOfConsultation(medicalRecordId: number): Observable<any> {
+    return this.http.get<any>(`${this.basePath}/medical-record/reasons-of-consultation/medicalRecordId/${medicalRecordId}`);
+  }
+
+  postReasonOfConsultation(data: {description: string, symptoms: string, medicalRecordId: number}): Observable<any> {
+    return this.http.post<any>(`${this.basePath}/medical-record/reasons-of-consultation`, data);
+  }
+}
